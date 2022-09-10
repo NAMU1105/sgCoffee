f2a2a793543faeb2452ad0b4331a32c346630608
namu1105 <nameunskadms@hanmail.net>
2022-09-10 21:20:07 +0800

    DELETE: unused ts-node library

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Div } from "./assets/styles/style";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Div className="App">메렁fdfd!</Div>;
+  return <Div className="App">메렁fdfd!dd</Div>;
 }
 
 export default App;
