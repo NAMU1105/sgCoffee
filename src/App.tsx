55cc66f4b4c27ea39b1f3539ad39764e9583c500
namu1105 <nameunskadms@hanmail.net>
2022-09-10 21:17:09 +0800

    DELETE: unused dependency libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import { Div } from "./assets/styles/style";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">메렁</div>;
+  return <Div className="App">메렁fdfd!</Div>;
 }
 
 export default App;
