ba7884d7112a51d4dbdcb9f67bfd5bef7b3805ec
namu1105 <nameunskadms@hanmail.net>
2022-09-12 00:22:15 +0800

    ADD: React router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
 import { Div } from "./assets/styles/style";
-import Counter from "./components/Component";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
-
   return (
     <Div className="App">
-      <Counter count={count} onDecrease={onDecrease} onIncrease={onIncrease} />
+      <Routes>
+        <Route path="/" element={<div>main</div>} />
+        <Route path="about" element={<div>about</div>} />{" "}
+      </Routes>
     </Div>
   );
 }
