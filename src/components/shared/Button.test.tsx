46997e8a3b8811c0b1391c377fa06017437538a3
Jonghak Seo <jonghak.seo@creatrip.com>
2022-09-18 18:47:31 +0900

    ADD: 버튼 테스트 추가

---
+++ src/components/shared/Button.test.tsx
@@ -0,0 +1,23 @@
+import {render} from "@testing-library/react";
+import Button from "./Button";
+
+
+describe('Button test', () => {
+    describe('loading test', () => {
+        test('로딩 상태일 경우 버튼은 disabled 가 된다', () => {
+            // given
+
+            // when
+            render(<Button/>)
+
+            // then
+        })
+        test('로딩 상태일 경우 버튼의 내용이 로딩 텍스트로 보인다', () => {
+            // given
+
+            // when
+
+            // then
+        })
+    })
+})
