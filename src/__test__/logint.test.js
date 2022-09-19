3bc8eab625eaa66edf7bcd0257a7889d86a31eae
namu1105 <nameunskadms@hanmail.net>
2022-09-19 23:57:04 +0800

    ADD: msw setting

---
+++ src/__test__/logint.test.js
@@ -0,0 +1,5 @@
+// test/Login.test.js
+
+test("allows user to log in", async () => {
+  // Render components, perform requests, receive mocked responses.
+});
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Start the mocking conditionally.
+if (process.env.NODE_ENV === "development") {
+  (async () => {
+    const { worker } = await import("./mocks/browser");
+    worker.start();
+  })();
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
